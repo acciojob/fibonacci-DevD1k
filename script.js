87ff581c1372e364030c7cd067cafd97a2b98407
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-04-23 19:54:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function fibonacci(num) {
 // your code here
-	let fib =[0,1]
+	let fib =[0,1];
 function fibonacci(num){
 
 	for(let i=2;i<num;i++){
 		fib[i] = fib[i-1] + fib[i-2];
 	}
 	return fib[num-1]
-}
 }
 
 module.exports = fibonacci;
